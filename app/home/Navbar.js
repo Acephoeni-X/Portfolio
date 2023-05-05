157a702ee4f101b9f203c5cee51b79c4d73ec4c1
Rishi Sharma <sharma.rishix02@gmail.com>
2023-05-05 10:55:22 +0530

    bahut saara kardia aur bhul gaya commit karna 😅

---
+++ app/home/Navbar.js
@@ -0,0 +1,181 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Navbar = (props) => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <>
+      <nav
+        className={
+          (props.transparent
+            ? "top-0 absolute z-50 w-full"
+            : "relative shadow-lg shadow-lg") +
+          " flex flex-wrap items-center justify-between px-2 py-3 dark:bg-gray-900"
+        }
+      >
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              href="/home"
+              className={
+                (props.transparent ? "text-white" : "text-gray-200") +
+                " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              }
+            >
+              Rishi Sharma
+            </Link>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none "
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-gray-700 lg:bg-transparent lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+            {/* <ul className="flex flex-col lg:flex-row list-none mr-auto">
+              <li className="flex items-center">
+                <a
+                  className={
+                    (props.transparent
+                      ? "lg:text-white lg:hover:text-gray-300 text-gray-200"
+                      : "text-gray-800 hover:text-gray-600") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/profile"
+                >
+                  <i
+                    className={
+                      (props.transparent
+                        ? "lg:text-gray-300 text-gray-500"
+                        : "text-gray-500") +
+                      " far fa-file-alt text-lg leading-lg mr-2"
+                    }
+                  />{" "}
+                  Docs
+                </a>
+              </li>
+            </ul> */}
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <Link
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-500 lg:hover:text-white text-gray-200"
+                      : "text-gray-200 hover:text-gray-200") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href={`${process.env.NEXT_PUBLIC_LINKEDIN}`}
+                  replace
+                >
+                  <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                  <span className="lg:hidden inline-block ml-2">twitter</span>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-500 lg:hover:text-white text-gray-200"
+                      : "text-gray-200 hover:text-gray-200") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href={`${process.env.NEXT_PUBLIC_LINKEDIN}`}
+                  replace
+                >
+                  <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span className="lg:hidden inline-block ml-2">Instagram</span>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-500 lg:hover:text-white text-gray-200"
+                      : "text-gray-200 hover:text-gray-200") +
+                    " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  }
+                  href={`${process.env.NEXT_PUBLIC_LINKEDIN}`}
+                  replace
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="23"
+                    height="23"
+                    viewBox="0 0 48 48"
+                    fill="currentColor"
+                  >
+                    <path d="M 16.5 5 C 10.16639 5 5 10.16639 5 16.5 L 5 31.5 C 5 37.832757 10.166209 43 16.5 43 L 31.5 43 C 37.832938 43 43 37.832938 43 31.5 L 43 16.5 C 43 10.166209 37.832757 5 31.5 5 L 16.5 5 z M 16.5 8 L 31.5 8 C 36.211243 8 40 11.787791 40 16.5 L 40 31.5 C 40 36.211062 36.211062 40 31.5 40 L 16.5 40 C 11.787791 40 8 36.211243 8 31.5 L 8 16.5 C 8 11.78761 11.78761 8 16.5 8 z M 34 12 C 32.895 12 32 12.895 32 14 C 32 15.105 32.895 16 34 16 C 35.105 16 36 15.105 36 14 C 36 12.895 35.105 12 34 12 z M 24 14 C 18.495178 14 14 18.495178 14 24 C 14 29.504822 18.495178 34 24 34 C 29.504822 34 34 29.504822 34 24 C 34 18.495178 29.504822 14 24 14 z M 24 17 C 27.883178 17 31 20.116822 31 24 C 31 27.883178 27.883178 31 24 31 C 20.116822 31 17 27.883178 17 24 C 17 20.116822 20.116822 17 24 17 z"></path>
+                  </svg>
+
+                  <span className="lg:hidden inline-block ml-2">Github</span>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link href="/Resume.pdf" download="RishiSharma">
+                  <button
+                    className={
+                      (props.transparent
+                        ? "bg-white text-gray-800 active:bg-gray-100"
+                        : "bg-pink-500 text-white active:bg-pink-600") +
+                      " text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                    }
+                    type="button"
+                    style={{ transition: "all .15s ease" }}
+                  >
+                    <i className="fas fa-arrow-alt-circle-down"></i> Download
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ pages/api/followings/index.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  let data = await (
+    await fetch(`https://api.github.com/users/UnspecifiedOne/following`, {
+      method: "GET",
+      headers: {
+        Authorization: `${process.env.GITHUB_TOKEN} `,
+      },
+    })
+  ).json();
+
+  res.status(200).json(data);
+}
