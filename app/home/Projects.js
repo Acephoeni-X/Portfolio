dda2441701971119cdb02f75b443dddb910b768f
Rishi Sharma <sharma.rishix02@gmail.com>
2023-05-05 22:55:04 +0530

    deploying ... 6

--- app/home/Projects.js
+++ app/home/Projects.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const getRepos = async () => {
   let data = await (
-    await fetch(`https://portfolio-rc6y.vercel.app//api/repos`, {
+    await fetch(`https://portfolio-rc6y.vercel.app/api/repos`, {
       next: { revalidate: 10 },
     })
   ).json();
