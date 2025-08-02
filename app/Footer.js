dbaca885cea75a1acfa14441f4133fb9fc54bdb9
Rishi Sharma <acephoenix@Rishis-MacBook-Air.local>
2025-08-02 12:31:50 +0530

    updated

--- app/Footer.js
+++ app/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="text-gray-600 body-font dark:bg-gray-700">
-      <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-300">
-          <span class="ml-3 text-xl">Rishi Sharma</span>
+    <footer className="text-gray-600 body-font dark:bg-gray-700">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-300">
+          <span className="ml-3 text-xl">Rishi Sharma</span>
         </a>
-        <p class="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+        <p className="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023 Rishi Sharma
         </p>
       </div>
