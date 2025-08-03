40d10dd758ea0dbb6768c6748c3ddd0a9915fe89
Rishi Sharma <acephoenix@Rishis-MacBook-Air.local>
2025-08-03 13:00:32 +0530

    fixed language icons not displaying

--- app/Projects.js
+++ app/Projects.js
@@ -1,6 +1,7 @@
 // "use client";
 import React from "react";
 import Link from "next/link";
+import Image from 'next/image'
 
 const getRepos = async () => {
   const token = process.env.GITHUB_TOKEN;
@@ -46,16 +47,17 @@ const getRepos = async () => {
 
 let lang = {
   Python:
-    "https://camo.githubusercontent.com/a1b2dac5667822ee0d98ae6d799da61987fd1658dfeb4d2ca6e3c99b1535ebd8/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f707974686f6e2d3336373041303f7374796c653d666f722d7468652d6261646765266c6f676f3d707974686f6e266c6f676f436f6c6f723d666664643534",
+    "/python.png",
   JavaScript:
-    "https://camo.githubusercontent.com/aeddc848275a1ffce386dc81c04541654ca07b2c43bbb8ad251085c962672aea/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145",
+    "/javascript.png",
   "C++":
-    "https://camo.githubusercontent.com/891c1fd9d2ab2adf1053e8514f469b94049769ccd9d2765c8e06e9c1b6da1b8c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632b2b2d2532333030353939432e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d63253242253242266c6f676f436f6c6f723d7768697465",
+    "/cpp.png",
   TypeScript:
-    "https://camo.githubusercontent.com/ee71fcc1aa3d059265517741dffc4161922fd744377e7a5f07c43381d0aa9aac/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f747970657363726970742d2532333030374143432e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d74797065736372697074266c6f676f436f6c6f723d7768697465",
-  Shell:
-    "https://camo.githubusercontent.com/7df250f624de398fbf14c84b1667c3d1e8e1450d67c0be03bd0dfb4e6a28c2c2/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f7368656c6c5f7363726970742d2532333132313031312e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d676e752d62617368266c6f676f436f6c6f723d7768697465",
-  HTML: "https://camo.githubusercontent.com/aeddc848275a1ffce386dc81c04541654ca07b2c43bbb8ad251085c962672aea/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145",
+    "/typescript.png",
+  Shell: "/shellscript.png",
+  HTML: "/html.png",
+  CSS: "/css.png",
+  Java: "/java.png"
 };
 // let getLanguages = async (json) => {
 //   let json_ = await getRepos();
@@ -100,20 +102,20 @@ const Projects = async () => {
                 <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-wrap">
                   {e.description}
                 </p>
-                {/* <ul className="flex space-x-4 sm:mt-0">
+                <ul className="flex space-x-4 sm:mt-0">
                   <li>
                     <a
                       href="#"
                       className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
                     >
                       {lang[e.language] ? (
-                        <img src={lang[e.language]} />
+                        <Image src={lang[e.language]} width={30} height={30} alt={e.language} />
                       ) : (
                         e.language
                       )}
                     </a>
                   </li>
-                </ul> */}
+                </ul>
               </div>
             </div>
           ))}
