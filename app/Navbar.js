2f2e3bd3c97b929563274b957950bdb00f8f6d43
Acephoeni-X <sharma.rishix02@gmail.com>
2025-08-30 08:13:44 +0530

    updated the dependency

--- app/Navbar.js
+++ app/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = (props) => {
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
-              href="/home"
+              href="/"
               className={
                 (props.transparent ? "text-white" : "text-gray-200") +
                 " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
